c1cf60e54d90ccf33d7d63f3a03a3ea9dbb3364c
maxsong1002 <maxsong1002@hotmail.com>
2013-10-11 17:43:41 +0900

    little fix to callerid

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -4,16 +4,17 @@ class CallsController < ApplicationController
         account_sid = 'AC836fb64c292cc774507c412443b7adb1'
         auth_token = '9085867319e82a04eb3a5c68ad7ce310'
         # This application sid will play a Welcome Message.
-        demo_app_sid = 'APabe7650f654fc34655fc81ae71caa3ff'
+        app_sid = 'AP4f310830a7a8eb025f0519bd997663c7'
         capability = Twilio::Util::Capability.new account_sid, auth_token
         capability.allow_client_outgoing "AP4f310830a7a8eb025f0519bd997663c7"
         capability.allow_client_incoming "internet_phone"
-        capability.allow_client_outgoing demo_app_sid #making a call to the demo_app
+        capability.allow_client_outgoing app_sid #making a call to the demo_app
         @token = capability.generate
     end
 
     def incoming_call
         number = params[:PhoneNumber]
+        caller_id = "+14702354516"
         response = Twilio::TwiML::Response.new do |r|
             # Should be your Twilio Number or a verified Caller ID
             r.Dial :callerId => caller_id do |d|
