2e043264cebeb08132c5a31b4eb619ec77b4cb93
maxsong1002 <maxsong1002@hotmail.com>
2013-10-13 12:09:09 +0900

    root changed. a web phone is finihsed

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -5,8 +5,6 @@ class CallsController < ApplicationController
         # Find these values at twilio.com/user/account
         account_sid = 'AC836fb64c292cc774507c412443b7adb1'
         auth_token = '9085867319e82a04eb3a5c68ad7ce310'
-        # This application sid will play a Welcome Message.
-        app_sid = 'AP4f310830a7a8eb025f0519bd997663c7'
         capability = Twilio::Util::Capability.new account_sid, auth_token
         capability.allow_client_outgoing "AP4f310830a7a8eb025f0519bd997663c7"
         capability.allow_client_incoming "internet_phone"
@@ -16,16 +14,15 @@ class CallsController < ApplicationController
     def outgoing_call
         number = params[:PhoneNumber]
         caller_id = "+14702354516"
-        response = Twilio::TwiML::Response.new do |r|
-           # Should be your Twilio Number or a verified Caller ID
+        response = Twilio::TwiML::Response.new do |r| # response to Twilio server's request
             r.Dial :callerId => caller_id do |d|
-                if /^[\d\+\-\(\) ]+$/.match(number)
-                    d.Number(CGI::escapeHTML number)
+                if /^[\d\+\-\(\) ]+$/.match(number) # if a valid number is given
+                    d.Number(CGI::escapeHTML number) # call the number
                 else
-                    d.Client 'internet_phone'
+                    d.Client 'internet_phone' # call the internet phone
                 end
             end
         end
-        render :text => response.text
+        render :text => response.text #return TwiML xml to send to Twilio server.
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ InternetPhone::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'calls#new'
 
   get 'call' => 'calls#new'
 
